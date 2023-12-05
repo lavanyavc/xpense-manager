e2dd44838e8d15ef9e7a0c8294d70954c33d5ef8
Lavanya VC <lavanya24octo@gmail.com>
2023-12-05 22:44:01 +0530

    added login page

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -1,15 +1,12 @@
-function updatedEmail() {
+function updatedEmail(input) {
       var mailId = document.getElementById("email");
-      var displayLetter = document.getElementsByClassName("circle");
-      mailId.value = mailId.value.toUpperCase();
-      mailId.result = mailId.value.charAt(0);
-      displayLetter[0].innerHTML = mailId.result;
-
+      var displayLetter = document.getElementById("logo");
+      var color = ["red", "blue", "yellow", "green", "orange", "cyan", "purple", "pink", "white"];
+      displayLetter.innerHTML = "Hi";
+      displayLetter.style.backgroundColor = "#eee";
       if (mailId.value.length > 0) {
-            return updatedEmail();
-      } else {
-            return hi;
+            let i = Math.floor(Math.random() * color.length - 1);
+            displayLetter.style.backgroundColor = color[i];
+            displayLetter.innerHTML = mailId.value.charAt(0).toUpperCase();
       }
-
 }
-
